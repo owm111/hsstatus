b6967470dd9d79daa8ca5ed549b9455af5147eca
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-08-09 15:30:10 -0500

    Clean up import list for Utils

--- src/HsStatus/Utils.hs
+++ src/HsStatus/Utils.hs
@@ -15,19 +15,19 @@ module HsStatus.Utils
   , hGetFirstLine
   ) where
 
-import Control.Exception
-import Control.Monad
-import Control.Monad.Zip
-import Data.Bifunctor hiding (second)
+import Control.Exception (Exception (..), try)
+import Control.Monad ((>=>))
+import Control.Monad.Zip (MonadZip (munzip))
+import Data.Bifunctor (first)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (pack, readInt)
 import qualified Data.ByteString as BS
 import Data.Word (Word8)
-import Data.Function
-import Data.Functor
-import System.IO.Error
+import Data.Function (on)
+import Data.Functor ((<&>))
+import System.IO.Error (IOError, tryIOError, userError)
 
-import HsStatus.IO
+import HsStatus.IO (Handle, SeekMode (..), hGetLine, hSeek)
 
 -- | @unzipWith@ lifted to a monad, i.e., a combination of 'munzip' and 'mapM'.
 -- Given a traversable and a function that returns a tuple, return a pair of
