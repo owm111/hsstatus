897b56d805affe56497fde7d2c713dc17fed082e
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-09-17 14:03:14 -0500

    Simplify batteryMonitor
    Runs via a timer instead of inotify, since inotify on batteries does
    not appear to be reliable at all. BattState now just acts as an enum
    and is paired with the capacity. Instead of doing division, the
    percent remaining is just read from capacity.

--- src/HsStatus/Fields/Battery.hs
+++ src/HsStatus/Fields/Battery.hs
@@ -1,95 +1,47 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 module HsStatus.Fields.Battery 
   ( BattState (..)
   , batteryMonitor
-  --, batteryMonitorFloating
-  , BattPaths (..)
-  , sysPowerSupply
   ) where
 
-import Control.Monad (liftM, liftM2)
-import Data.ByteString (ByteString, hGetLine)
-import Data.ByteString.Char8 (pack)
-import Data.Functor ((<&>))
-import System.INotify (EventVariety (Access, Modify))
-import System.IO (IOMode (ReadMode), openFile, withFile)
-import System.IO.Error (IOError, tryIOError)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TSem
+import Control.Monad
+import System.IO
 
-import HsStatus.FieldUtils (iNotifyWatcher)
-import HsStatus.Types.Field (Field (..))
-import HsStatus.Utils (hGetFirstLine, packExceptions, readIntEither)
+import HsStatus.Types.Field
 
-newtype BattPaths = BattPaths (String, String, String, String)
-
-sysPowerSupply :: String -> String -> String -> BattPaths
-sysPowerSupply name now full =
-  let dir = "/sys/class/power_supply/" ++ name ++ "/"
-  in BattPaths (dir ++ "status", dir ++ now, dir ++ full, dir ++ "uevent")
-
-data BattState n
-  = Discharging n
-  | Charging n
-  | NotCharging n
+data BattState
+  = Discharging
+  | Charging
+  | NotCharging
   | Full
   | Unknown
+  deriving (Eq, Show, Read)
 
-makeState :: IO (Either IOError ByteString) -> IO (Either IOError a) -> IO (Either IOError (BattState a))
-makeState = liftM2 toStateEither
-  where toStateEither = liftM2 toStateF
+hRewind :: Handle -> IO ()
+hRewind h = hSeek h AbsoluteSeek 0
 
-        toStateF "Charging" = Charging
-        toStateF "Discharging" = Discharging
-        toStateF "Not charging" = NotCharging
-        toStateF "Full" = const Full
-        toStateF _ = const Unknown
-{-
--- | Field that displays status and percent remaining of battery.
---
--- TODO: close handles?
-batteryMonitorFloating :: BattPaths -> Int -> IO (Field (BattState Double))
-batteryMonitorFloating (BattPaths (status, now, full, uevent)) digits = do
-  fullN   <- tryIOError (withFile full ReadMode hGetLine) <&> readIntEither
-  statusH <- tryIOError (openFile status ReadMode)
-  nowH    <- tryIOError (openFile now ReadMode)
+statusPair :: Char -> IO Int -> IO (BattState, Int)
+statusPair 'F' _ = pure (Full, 100)
+statusPair 'C' n = (Charging,)    <$> n
+statusPair 'D' n = (Discharging,) <$> n
+statusPair 'N' n = (NotCharging,) <$> n
+statusPair  _  _ = pure (Unknown, 0)
 
-  let makePercent :: Either IOError Int -> Either IOError Double
-      makePercent = liftM (/ (10^digits)) . liftM fromIntegral . liftM2 (flip div) fullN . liftM (* (10^(digits + 2)))
-
-      getStatus :: IO (Either IOError ByteString)
-      getStatus = hGetFirstLine statusH
-
-      getPercent :: IO (Either IOError Double)
-      getPercent = hGetFirstLine nowH <&> readIntEither <&> makePercent
-
-      go :: a -> IO (Either ByteString (BattState Double))
-      go _ = makeState getStatus getPercent <&> packExceptions
-
-      events = [([Modify, Access], pack uevent)]
-
-  return (iNotifyWatcher events go)
--}
--- TODO: just use capacity file
-batteryMonitor :: BattPaths -> IO (Field (BattState Int))
-batteryMonitor (BattPaths (status, now, full, uevent)) = do
-  fullN   <- tryIOError (withFile full ReadMode hGetLine) <&> readIntEither
-  statusH <- tryIOError (openFile status ReadMode)
-  nowH    <- tryIOError (openFile now ReadMode)
-
-  let makePercent :: Either IOError Int -> Either IOError Int
-      makePercent = liftM2 (flip div) fullN . liftM (*100)
-
-      getStatus :: IO (Either IOError ByteString)
-      getStatus = hGetFirstLine statusH
-
-      getPercent :: IO (Either IOError Int)
-      getPercent = hGetFirstLine nowH <&> readIntEither <&> makePercent
-
-      go :: a -> IO (BattState Int)
-      go _ = makeState getStatus getPercent <&> packExceptions <&> (\(Right x) -> x)
-
-      -- TODO: since I'm watching uevent, might it be faster to have a single
-      -- handle open and just parse this file?
-      events = [([Modify, Access], pack uevent)]
-
-  return (iNotifyWatcher events Unknown go)
+batteryMonitor :: String -> Int -> Field (BattState, Int)
+batteryMonitor name delay =
+  let status   = "/sys/class/power_supply/" ++ name ++ "/status"
+      capacity = "/sys/class/power_supply/" ++ name ++ "/capacity"
+      f printSem var = do
+        statusH <- openFile status ReadMode
+        capacityH <- openFile capacity ReadMode
+        fmap (:[]) . forkIO . forever $ do
+          statusC <- hGetChar statusH
+          hRewind statusH
+          pair <- statusPair statusC (readIO =<< hGetLine capacityH <* hRewind capacityH)
+          atomically (writeTVar var pair >> signalTSem printSem)
+          threadDelay delay
+   in Field ((Unknown, 0), f)
