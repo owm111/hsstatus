ee75d3a8e7249e8ad6263575da70aec739c6954c
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-09-17 10:34:45 -0500

    Date field works like strftime(..., localtime(t))
    Instead of returning a time object, a formatted string is returned.
    The field itself is does not return in IO.

--- src/HsStatus/Fields/Date.hs
+++ src/HsStatus/Fields/Date.hs
@@ -1,6 +1,5 @@
 module HsStatus.Fields.Date
   ( dateField
-  , dateFieldInZone
   ) where
 
 import Data.Time
@@ -8,15 +7,6 @@ import Data.Time
 import HsStatus.FieldUtils (runEvery)
 import HsStatus.Types.Field (Field (..))
 
-getDateInZone :: TimeZone -> IO ZonedTime
-getDateInZone zone = utcToZonedTime zone <$> getCurrentTime
-
--- | 'dateFieldInZone' for the current timezone.
-dateField :: Int -> IO (Field ZonedTime)
-dateField delay = do
-  zone <- getCurrentTime >>= getTimeZone
-  dateFieldInZone zone delay
-
--- | Field that displays the current time within a given timezone.
-dateFieldInZone :: TimeZone -> Int -> IO (Field ZonedTime)
-dateFieldInZone zone delay = pure $ runEvery delay (ZonedTime (LocalTime (ModifiedJulianDay 0) (TimeOfDay 0 0 0)) zone) (getDateInZone zone)
+dateField :: Int -> String -> Field String
+dateField delay format = runEvery delay ""
+  (formatTime defaultTimeLocale format <$> getCurrentTime)
