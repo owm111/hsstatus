825b9f295a512af97a8eabac2e96c8a04214b404
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-09-17 10:58:54 -0500

    Simplify brightnessMonitor
    brightnessMonitor was rewritten. Exception handling was removed. INotify now
    watches for CloseWrite, since from my testing, Modify appears to be called
    twice when the brightness is changed, but CloseWrite is only called once.
    
    A "lock" was added so only one thread can access the brightness file at a time.
    Not sure if this is needed, but it looks like it could prevent two getLines
    before it was seeked back the beginning.
    
    Instead of using custom types for specifying paths, just plain Strings and Ints
    are used.

--- src/HsStatus/Fields/Brightness.hs
+++ src/HsStatus/Fields/Brightness.hs
@@ -1,57 +1,34 @@
 module HsStatus.Fields.Brightness
-  ( BrightState (..)
-  , brightnessMonitor
-  --, brightnessMonitorFloating
-  , BrightPaths (..)
-  , sysBacklight
+  ( brightnessMonitor
   ) where
 
-import Control.Monad (liftM, liftM2)
-import Data.ByteString (ByteString, hGetLine)
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TSem
 import Data.ByteString.Char8 (pack)
-import Data.Functor ((<&>))
-import System.INotify (EventVariety (Modify))
-import System.IO (IOMode (ReadMode), openFile, withFile)
-import System.IO.Error (IOError, tryIOError)
+import System.INotify
+import System.IO
 
-import HsStatus.FieldUtils (iNotifyWatcher)
-import HsStatus.Types.Field (Field (..))
-import HsStatus.Utils (hGetFirstLine, packExceptions, readIntEither)
+import HsStatus.Types.Field
 
-newtype BrightPaths = BrightPaths (String, String)
-
-sysBacklight :: String -> String -> String -> BrightPaths
-sysBacklight name bright max =
-  let dir = "/sys/class/backlight/" ++ name ++ "/"
-  in BrightPaths (dir ++ bright, dir ++ max)
-
-newtype BrightState a = BrightState a
-
--- | Field that monitors the brightness of a display.
-brightnessMonitor :: BrightPaths -> IO (Field (BrightState Int))
-brightnessMonitor (BrightPaths (bright, maxbright)) = do
-  maxN <- tryIOError (withFile maxbright ReadMode hGetLine) <&> readIntEither <&> (\(Right x) -> x)
-  nowH <- tryIOError (openFile bright ReadMode)
-
-  let makePercent :: Int -> Int
-      makePercent = (flip div) maxN . (*100)
-
-      go :: a -> IO (BrightState Int)
-      go _ = hGetFirstLine nowH <&> readIntEither <&> (\(Right x) -> x) <&> makePercent <&> BrightState
-
-  return (iNotifyWatcher [([Modify], pack bright)] (BrightState 0) go)
-
-{-
-brightnessMonitorFloating :: BrightPaths -> Int -> IO (Field (BrightState Double))
-brightnessMonitorFloating (BrightPaths (bright, maxbright)) digits = do
-  maxN <- tryIOError (withFile maxbright ReadMode hGetLine) <&> readIntEither
-  nowH <- tryIOError (openFile bright ReadMode)
-
-  let makePercent :: Either IOError Int -> Either IOError Double
-      makePercent = liftM (/ (10^digits)) . liftM fromIntegral . liftM2 (flip div) maxN . liftM (* (10^(digits + 2)))
-
-      go :: a -> IO (Either ByteString (BrightState Double))
-      go _ = hGetFirstLine nowH <&> readIntEither <&> makePercent <&> liftM BrightState <&> packExceptions
-
-  return (iNotifyWatcher [([Modify], pack bright)] go)
-  -}
+brightnessMonitor :: String -> Field Int
+brightnessMonitor name =
+  let brightness = "/sys/class/backlight/" ++ name ++ "/brightness"
+      max_brightness = "/sys/class/backlight/" ++ name ++ "/max_brightness"
+      go printSem var = do
+        locked <- newEmptyMVar
+        max <- withFile max_brightness ReadMode hGetLine >>= readIO
+        briH <- openFile brightness ReadMode
+        let toPerc bri = bri * 100 `div` max
+            update = do
+              didLock <- tryPutMVar locked ()
+              when didLock $ do
+                bri <- hGetLine briH >>= readIO
+                hSeek briH AbsoluteSeek 0
+                tryTakeMVar locked
+                atomically (writeTVar var (toPerc bri) >> signalTSem printSem)
+        update
+        inot <- initINotify
+        [] <$ addWatch inot [CloseWrite] (pack brightness) (const update)
+   in Field (0, go)
