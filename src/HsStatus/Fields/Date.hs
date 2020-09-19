bc493c18a68131c40208e638829c820d3d19de0b
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-09-19 16:30:44 -0500

    Rewrite dateField without FieldUtils

--- src/HsStatus/Fields/Date.hs
+++ src/HsStatus/Fields/Date.hs
@@ -4,9 +4,21 @@ module HsStatus.Fields.Date
 
 import Data.Time
 
-import HsStatus.FieldUtils (runEvery)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TSem
+import Control.Monad
+import Data.Time
+
 import HsStatus.Types.Field (Field (..))
 
 dateField :: Int -> String -> Field String
-dateField delay format = runEvery delay ""
-  (formatTime defaultTimeLocale format <$> getCurrentTime)
+dateField delay format =
+  let formatF = formatTime defaultTimeLocale format
+      go printSem var = do
+        tid <- forkIO . forever $ do
+          t <- formatF <$> getCurrentTime
+          atomically (writeTVar var t >> signalTSem printSem)
+          threadDelay delay
+        return [tid]
+   in Field ("", go)
