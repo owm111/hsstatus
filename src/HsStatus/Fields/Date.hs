9fb27f8ae7181ceef61a066e1f52071f2ce2915f
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-09-27 20:16:44 -0500

    Replace calls to time with calls to C time.h
    More performance, but it feels like a hack. Oh well.

--- src/HsStatus/Fields/Date.hs
+++ src/HsStatus/Fields/Date.hs
@@ -2,22 +2,43 @@ module HsStatus.Fields.Date
   ( dateField
   ) where
 
-import Data.Time
-
 import Control.Concurrent
 import Control.Monad
-import Data.Time
 import Data.IORef
+import Foreign
+import Foreign.C
 
 import HsStatus.Types.Field (Field (..))
 
 dateField :: Int -> String -> Field String
-dateField delay format = Field $ \printSem _ var -> do
-  let formatF = formatTime defaultTimeLocale format
-  forever $ do
-    t <- formatF <$> getCurrentTime
-    writeIORef var t
-    tryPutMVar printSem ()
-    threadDelay delay
+dateField delay format = Field $ \printSem _ var ->
+  withCString format $ \fmt ->
+    alloca $ \timePtr ->
+      allocaArray0 128 $ \strPtr ->
+        allocaBytesAligned 56 8 $ \tmPtr ->
+          forever $ do
+            c_time timePtr
+            c_localtime_r timePtr tmPtr
+            c_strftime strPtr 128 fmt tmPtr
+            writeIORef var =<< peekCString strPtr
+            tryPutMVar printSem ()
+            threadDelay delay
 
 {-# INLINE dateField #-}
+
+newtype CTm = CTm (Ptr CTm)
+
+instance Storable CTm where
+  sizeOf _ = 56
+  alignment _ = 8
+  peek = undefined
+  poke = undefined
+
+foreign import ccall unsafe "time.h time"
+  c_time :: Ptr CTime -> IO CTime
+
+foreign import ccall unsafe "time.h localtime_r"
+  c_localtime_r :: Ptr CTime -> Ptr CTm -> IO (Ptr CTm)
+
+foreign import ccall unsafe "time.h strftime"
+  c_strftime :: CString -> CSize -> CString -> Ptr CTm -> IO CSize
